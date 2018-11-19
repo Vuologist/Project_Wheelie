9cc3522ac4421ac776054775c9c0464886463904
Anthony Vu <tk42111@gmail.com>
2018-11-19 09:59:51 -0800

    read in data from local json

--- app/components/DailyMeals/DailyMeals.js
+++ app/components/DailyMeals/DailyMeals.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 import "./styles";
 
-const DailyMeals = () => {
-  return (
-    <View>
-      <Text>Hello there</Text>
-    </View>
-  );
-};
+class DailyMeals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: []
+    };
+  }
+
+  componentDidMount() {
+    var data = require("../../data/DailyMeals.json");
+    console.log(data.monday[0]);
+    this.setState({
+      dataSource: data.monday
+    });
+  }
+
+  renderItems = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View>
+        <FlatList data={this.state.dataSource} renderItem={this.renderItems} />
+      </View>
+    );
+  }
+}
 
 export default DailyMeals;
